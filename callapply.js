dd84ced10d6cc7724285216c5ab949f49cb67c07
unknown <anwarhosen575@gmail.com>
2021-02-21 22:17:41 +0600

    abar

--- callapply.js
+++ callapply.js
@@ -3,13 +3,31 @@ const normalPerson = {
     lastName: 'Uddin',
     salary: 15000,
     getFulName: function(){
-        
+       console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this)
         this.salary = this.salary - amount;
         return this.salary;
+       
     }
+    
 }
 
-normalPerson.chargeBill(2500);
-console.log(normalPerson.salary);
+const heroPerson = {
+    firstName: 'Hero',
+    lastName:'Balam',
+    salary:25000,
+}
+
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName:'Golam',
+    salary:25000,
+}
+
+const herochargeBill = normalPerson.chargeBill.bind(heroPerson);
+herochargeBill(2000);
+herochargeBill(3000);
+herochargeBill(5000);
+console.log(heroPerson.salary);
